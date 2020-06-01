f43aaf941f32156e0239fa334d05317d79dc0c70
kielkow <matheuskiel@fiorifer.com.br>
2020-06-01 18:45:08 -0300

    Go_lint adjusts

--- database/database.go
+++ database/database.go
@@ -5,12 +5,14 @@ import (
 	"log"
 )
 
+// DbConn to conect on database
 var DbConn *sql.DB
 
+// SetupDatabase to conect on database
 func SetupDatabase() {
 	var err error
 
-	DbConn, err = sql.Open("mysql", "root:password123@tcp(127.0.0.1:5432)/inventorydb")
+	DbConn, err = sql.Open("mysql", "root:password123@tcp(127.0.0.1:3006)/inventorydb")
 
 	if err != nil {
 		log.Fatal(err)
